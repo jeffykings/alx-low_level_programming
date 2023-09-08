5b66bebbd83e1f7f46a0ac1ac13b265cb8907297
Amuh Emeka <99865958+jeffykings@users.noreply.github.com>
2023-09-07 20:08:19 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point of program
+ * Return: it returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
