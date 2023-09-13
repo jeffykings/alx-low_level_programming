6a46aea6645f06e1f4cc23921ff093984d49abbe
Amuh Emeka <99865958+jeffykings@users.noreply.github.com>
2023-09-12 20:12:50 -0700

    task1 bug fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,28 +1,15 @@
 #include "main.h"
 
 /**
-  * main- entry point of the ptogram and calls the print_alpabet function
-  * Return: 0 is always success
-  */
-
-int main(void)
-{
-
-	print_alphabet();
-
-	return (0);
-}
-
-/**
   * print_alphabet- prints all lower case letters
   * Return: doesnt return any value
   */
 
 void print_alphabet(void)
 {
-	int i = 0;
+	char i = 'a';
 
-	while (i <= 122)
+	while (i <= 'z')
 	{
 		_putchar(i);
 		i++;
