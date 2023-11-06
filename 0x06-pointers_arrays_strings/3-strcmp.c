d9cdb7f540b06837ff4fdaa0d9618ebfdbd02daa
Amuh Emeka <99865958+jeffykings@users.noreply.github.com>
2023-11-06 10:32:24 -0800

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,36 @@
+/**
+  * _strcmp -  compares two strings.
+  * @s1: pointer to string 
+  * @s2: pointe to string
+  * Return: integer
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	if (s1 != s2)
+	{
+		for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+		{
+			if (s1[i] == s2[i])
+			{
+				if ((s1[i] == '\0') && (s2[i] == '\0'))
+				{
+					return (0);
+				}
+
+				continue;
+			}
+			else if (s1[i] > s2[i])
+			{
+				return(15);
+			}
+			else
+			{
+				return(-15);
+			}
+		}
+	}
+	return (0);
+}
