eba57fe4aaa2f031ff204ea4de8734bfb4c213ed
Amuh Emeka <99865958+jeffykings@users.noreply.github.com>
2023-11-06 08:40:29 -0800

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+/**
+  * _strncpy -  copies a string.
+  * @dest: pointer to a string
+  * @src: pointer to a string
+  * @n: checks how many strings to copy
+  * Return: pointer to a string
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+			dest[j] = src[j];
+	}
+
+	for (; j < n; j++)
+	{
+		dest[j] = '\0';
+	}
+
+	return (dest);
+}
