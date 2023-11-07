30616563035bb3b105ddcdefa8628eeb79feecc5
Amuh Emeka <99865958+jeffykings@users.noreply.github.com>
2023-11-07 05:53:22 -0800

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+/**
+  * string_toupper - changes all lowercase letters of a string to uppercase.
+  * @str: pointer to a string to be changed
+  * Return: pointer to a string
+  */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 97) && (str[i] <= 122))
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
