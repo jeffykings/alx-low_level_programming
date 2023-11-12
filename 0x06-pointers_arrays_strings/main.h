0c50948042673f32e3213aa91313bc0166faa5a1
Amuh Emeka <99865958+jeffykings@users.noreply.github.com>
2023-11-12 13:43:22 -0800

    task103

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,4 +12,6 @@ char *leet(char *);
 char *rot13(char *);
 void print_digit(int n);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void rev_string(char *n);
 #endif 
