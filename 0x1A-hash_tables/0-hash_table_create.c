712f219e4a7963835918afafc5834873af947650
Amuh Emeka <99865958+jeffykings@users.noreply.github.com>
2023-12-22 07:55:06 -0800

    task0 update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *ptr;
 	unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	ptr = malloc(sizeof(hash_table_t));
+	if (ptr == NULL)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	ptr->size = size;
+	ptr->array = malloc(sizeof(hash_node_t *) * size);
+	if (ptr->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+		ptr->array[i] = NULL;
 
-	return (ht);
+	return (ptr);
 }
