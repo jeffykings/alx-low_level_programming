5c6460aabecd338b3476c36bbefbab36dcd67ad5
Amuh Emeka <99865958+jeffykings@users.noreply.github.com>
2023-12-22 16:02:50 -0800

    task3 update

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <unistd.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -8,4 +13,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+unsigned int binary_to_dec(const int *b, unsigned int size);
+int size_of_bin(unsigned long int n);
 #endif
