5ea0137350742f25e55d527268f28e7fe15beebc
Amuh Emeka <99865958+jeffykings@users.noreply.github.com>
2023-12-24 03:13:35 -0800

    task0 Update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x14-file_io directory.
  */
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -17,4 +18,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-
+#endif
