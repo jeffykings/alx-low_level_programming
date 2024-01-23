96308540948653d6f6cd27bf4d9ac5ff0d28cf29
JK7 <jeffykings@gmail.com>
2024-01-23 13:19:20 +0100

    task100 0x18

---
+++ 0x18-dynamic_libraries/arithemitcs.c
@@ -0,0 +1,55 @@
+/**
+ * add - addition of two numbers
+ * @a: first value
+ * @b: second value
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtraction of two numbers
+ * @a: first value
+ * @b: second value
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - muktiplication of two numbers
+ * @a: first value
+ * @b: second value
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two values
+ * @a: dividend
+ * @b: divisor
+ * Return: the result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - gets the remainder of a division
+ * @a: dividend
+ * @b: divisor
+ * Return: Result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
