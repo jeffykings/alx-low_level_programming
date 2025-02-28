872027c999f55ff6f13f4bcd1002135cf0e4474d
jeffykings <jeffykings@gmail.com>
2025-02-28 13:09:59 +0100

    task100 part save

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -208,3 +208,40 @@ void shandle_collision(shash_table_t *ht, shash_node_t *hash_element,
 		ht->array[idx] = hash_element;
 	}
 }
+
+/**
+ * hash_table_get - a function that retrieves a value associated with a key
+ *
+ * @ht:   is the hash table you want to look into
+ * @key: is the key you are looking for
+ *
+ * Return:  value associated with the element, or NULL
+ * if key couldn’t be found
+ */
+char *shash_table_get(const hash_table_t *ht, const char *key)
+{
+	shash_node_t *temp;
+	unsigned long int idx;
+
+	if (ht == NULL)
+		return (NULL);
+
+	idx = key_index((const unsigned char *) key, ht->size);
+
+	if (ht->array[idx] != NULL)
+	{
+		temp = ht->array[idx];
+		while  (temp->next)
+		{
+			if (strcmp(temp->key, key) == 0)
+				return (temp->value);
+
+			temp = temp->next;
+		}
+
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+	}
+
+	return (NULL);
+}
